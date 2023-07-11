babad7fce38b1a7944c437af474a613e984df0ef
Ma7moud3wd <mahmooodawd@gmail.com>
2023-07-11 22:15:32 +0300

    Check Network Connectivity

--- app/src/main/java/com/example/marsad/ui/MainActivity.kt
+++ app/src/main/java/com/example/marsad/ui/MainActivity.kt
@@ -1,24 +1,50 @@
 package com.example.marsad.ui
 
+import android.Manifest
+import android.app.StatusBarManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.PatternMatcher
+import android.provider.Settings
+import android.view.View
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.marsad.R
 import com.example.marsad.databinding.ActivityMainBinding
+import com.example.marsad.ui.utils.NetworkConnectivityObserver
 import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
+    lateinit var networkConnectivityObserver: NetworkConnectivityObserver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        networkConnectivityObserver = NetworkConnectivityObserver(applicationContext)
+        lifecycleScope.launch {
+            networkConnectivityObserver.netState.collect {
+                if (it != NetworkConnectivityObserver.Status.Available) {
+                    Snackbar.make(binding.root, getString(R.string.no_internet_msg), Snackbar.LENGTH_LONG).show()
+                }
+            }
+        }
+        networkConnectivityObserver.getNetState()
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
 
@@ -41,8 +67,6 @@ class MainActivity : AppCompatActivity() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             supportActionBar?.title = destination.label
         }
+
     }
-
-
 }
-
---
+++ app/src/main/java/com/example/marsad/ui/utils/NetworkConnectivityObserver.kt
@@ -0,0 +1,48 @@
+package com.example.marsad.ui.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import kotlinx.coroutines.flow.*
+
+class NetworkConnectivityObserver(context: Context) {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private val networkRequest = NetworkRequest.Builder()
+        .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+        .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+        .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+        .build()
+    private var _netState = MutableStateFlow(Status.UnAvailable)
+    val netState = _netState.asStateFlow()
+
+    @SuppressLint("MissingPermission")
+    fun getNetState() {
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                _netState.value = Status.Available
+            }
+
+            override fun onUnavailable() {
+                super.onUnavailable()
+                _netState.value = Status.UnAvailable
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                _netState.value = Status.Lost
+            }
+        }
+
+        connectivityManager.registerNetworkCallback(networkRequest, callback)
+
+    }
+
+    enum class Status {
+        Available, UnAvailable, Lost
+    }
+}
